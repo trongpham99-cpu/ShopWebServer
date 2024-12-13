8413a9cecae8db1fe1d780c0b2f7e2f95db2604b
trong.phamtranduc@gmail.com <trong.phamtranduc@gmail.com>
2024-12-14 00:24:44 +0700

    update

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
 });
