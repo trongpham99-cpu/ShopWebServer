1a18585c2bedcefcba8e1ba3f0c0a1ef6155cc56
trong.phamtranduc@gmail.com <trong.phamtranduc@gmail.com>
2024-12-14 00:29:15 +0700

    add more endpoint /about

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
